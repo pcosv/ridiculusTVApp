3c4510e28582424e2b90eb724b6bd5fc23ad5936
René Melo de Lucena <rml7@academy.cin.ufpe.br>
2018-09-18 14:50:30 -0300

    first commit

---
+++ ridiculusTVApp/ridiculusTVApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ridiculusTVApp
+//
+//  Created by René Melo de Lucena on 18/09/2018.
+//  Copyright © 2018 René Melo de Lucena. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
